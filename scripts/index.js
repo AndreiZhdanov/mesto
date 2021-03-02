fedfaa9c9711ad1e62d667f2cc109325f3cea572
Andrei Zhdanov <andreizhdanov@iMac-Andrei-2.local>
2021-03-02 13:26:40 +0300

    доработаны стили, исправлен popup, отредактирован js

--- scripts/index.js
+++ scripts/index.js
@@ -3,27 +3,27 @@ let popup = document.querySelector('.popup');
 let buttonReset = document.querySelector('.popup__button_close');
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
-let popupContainer = document.querySelector('.popup__container');
 let fieldName = document.querySelector('#name');
 let fieldAbout = document.querySelector('#about');
+let form = document.querySelector('#user-form');
 
 function showPopup() {
   popup.classList.add('popup_opened');
   fieldName.value = profileName.textContent;
-  fieldAbout.value = profileAbout.texr.textContent;
+  fieldAbout.value = profileAbout.textContent;
 }
 
-function popupReset() {
+function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
 function editProfileInfo(event) {
   event.preventDefault();
   profileName.textContent = fieldName.value;
-  profileAbout = fieldAbout.value;
-  popupReset();
+  profileAbout.textContent = fieldAbout.value;
+  closePopup();
 }
 
 buttonEdit.addEventListener('click', showPopup);
-buttonReset.addEventListener('click', popupReset);
+buttonReset.addEventListener('click', closePopup);
 form.addEventListener('submit', editProfileInfo);
